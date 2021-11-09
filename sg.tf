52f3d7cf6ca93d6ebfeeff3e7930d4723f1a4809
Ankit-Varshney-devops <85929550+Ankit-Varshney-devops@users.noreply.github.com>
2021-11-10 01:30:46 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -74,7 +74,7 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    security_groups = [aws_security_group.alb_web_sg.id]
+    security_groups = [aws_security_group.asg_web_sg.id]
   }
 
   egress {
